99193ed4f372f3690cd80cb1fa95d56acaf1a8fa
Spring <Springsquash93@gmail.com>
2023-04-29 10:35:12 -0400

    Fix navbar background clipping

--- src/App.js
+++ src/App.js
@@ -7,16 +7,16 @@ function App() {
       <div className="flex">
 
         {/* Side Panel */}
-        <div className="h-screen w-auto bg-yellow-500">
-          <SidePanel />
-        </div>
+          <div className="h-screen">
+            <SidePanel />
+          </div>
 
         {/* Main */}
-        <div className="flex h-screen w-full flex-col">
+        <div className="flex w-full flex-col h-screen overflow-x-hidden">
 
           {/* Header */}
-          <div className="flex min-h-[50vh] h-min bg-paper-pattern bg-blend-multiply">
-            <div className="backdrop-filter backdrop-blur-lg h-full w-full flex justify-center items-center">
+          <div className="flex min-h-[50vh] h-min bg-paper-pattern bg-blend-multiply flex-3">
+            <div className="backdrop-filter backdrop-blur-sm h-full w-full flex justify-center items-center">
               <div className="w-60 h-60 bg-blue-500 rounded-full animate-blob translate-x-5 animation-delay-4000 filter blur-md opacity-50 shadow-lg mix-blend-multiply shadow-white"></div>
               <div className="absolute w-60 h-60 bg-pink-500 rounded-full animate-blob filter blur-md opacity-50 shadow-lg mix-blend-multiply shadow-white"></div>
               <div className="w-60 h-60 bg-orange-500 rounded-full animate-blob animation-delay-2000 filter blur-md opacity-50 shadow-lg mix-blend-multiply shadow-white"></div>
@@ -24,15 +24,17 @@ function App() {
                 <h1 className="text-8xl font-light">SOCIAL FLOW</h1>
                 <h3 className="">I am a catchy slogan...</h3>
               </div>
-
             </div>
           </div>
 
           {/* Body */}
-          <div className="h-full bg-purple-300">
+          <div className="bg-purple-300 flex-1">
+            <div className="m-12 backdrop-filter bg-purple-400 rounded-3xl shadow-white shadow-2xl">
+              <div className="flex flex-wrap justify-center items-center">
 
+              </div>
+            </div>
           </div>
-
         </div>
       </div>
     </div>
