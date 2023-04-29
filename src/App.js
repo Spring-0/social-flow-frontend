dd2be10f9242d211285b07afa93ed6a0673fd931
spring <springsquash93@gmail.com>
2023-04-29 02:26:37 -0400

    Fix structure & layout

--- src/App.js
+++ src/App.js
@@ -4,33 +4,36 @@ import Card from "./components/Card";
 function App() {
   return (
     <div className="App">
-      <div className="z-20 fixed">
-        <SidePanel />
-      </div>
+      <div className="flex">
 
-      <div className=" h-screen bg-purple-200">
+        {/* Side Panel */}
+        <div className="h-screen w-auto bg-yellow-500">
+          <SidePanel />
+        </div>
 
-        <div className="bg-paper-pattern bg-blend-multiply w-screen h-[65%]">
-          <div className="flex justify-center align-top w-screen h-full backdrop-filter backdrop-blur-md">
-            <div className="relative flex border border-black items-center w-full justify-center">
+        {/* Main */}
+        <div className="flex h-screen w-full flex-col">
+
+          {/* Header */}
+          <div className="flex min-h-[50vh] h-min bg-paper-pattern bg-blend-multiply">
+            <div className="backdrop-filter backdrop-blur-lg h-full w-full flex justify-center items-center">
               <div className="w-60 h-60 bg-blue-500 rounded-full animate-blob translate-x-5 animation-delay-4000 filter blur-md opacity-50 shadow-lg mix-blend-multiply shadow-white"></div>
               <div className="absolute w-60 h-60 bg-pink-500 rounded-full animate-blob filter blur-md opacity-50 shadow-lg mix-blend-multiply shadow-white"></div>
               <div className="w-60 h-60 bg-orange-500 rounded-full animate-blob animation-delay-2000 filter blur-md opacity-50 shadow-lg mix-blend-multiply shadow-white"></div>
-              <h1 className="absolute text-8xl text-white font-light">SOCIAL FLOW</h1>
+              <div className="absolute text-center text-white">
+                <h1 className="text-8xl font-light">SOCIAL FLOW</h1>
+                <h3 className="">I am a catchy slogan...</h3>
+              </div>
+
             </div>
           </div>
 
-          <div className="flex justify-center">
-            <div className="m-3">
-              {/* Cards go here */}
-              <Card />
-            </div>
+          {/* Body */}
+          <div className="h-full bg-purple-300">
+
           </div>
+
         </div>
-
-
-
-
       </div>
     </div>
   );
